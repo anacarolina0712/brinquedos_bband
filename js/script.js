55e11f3c5af266a531c858be7512043cdc965f7c
ana carolina <aalvesdacostamoura@gmail.com>
2022-12-06 12:05:40 -0300

    forms

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// var tab1 = document.querySelector('#icone');
+// var content =document.querySelector('.content')
+
+// tab1.addEventListener('click', function(){
+//     if(content.style.display === 'block') {
+//         content.style.display = 'none';
+//     }else {
+//     content.style.display = 'block';
+// }
+// });
+
+function login(){
+    window.location.href="home.html";
+
+}
